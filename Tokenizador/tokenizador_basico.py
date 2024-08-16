6177a97d77d7ce7da94220722ff7cf506b0134a6
Luis Gabriel Martinez Contreras <93987217+Luismartco@users.noreply.github.com>
2024-08-16 15:08:17 +0000

    tokenbasico

--- Tokenizador/tokenizador_basico.py
+++ Tokenizador/tokenizador_basico.py
@@ -3,7 +3,6 @@ nltk.download('punkt')
 from nltk.tokenize import word_tokenize
 texto = 'No es lo mismo ser ciego que caminar la vida con ojos vendados'
 token1 = word_tokenize(texto)
-#token.word_tokenize(texto)
 len(texto)
 print(token1)
 print(len(token1))
