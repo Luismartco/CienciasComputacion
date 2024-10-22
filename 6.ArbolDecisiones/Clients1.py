474592e26515d332cb9b1870c0c12d196a0cb65a
Luis Gabriel Martinez Contreras <93987217+Luismartco@users.noreply.github.com>
2024-10-22 15:55:12 +0000

    normalizacion

--- 6.ArbolDecisiones/Clients1.py
+++ 6.ArbolDecisiones/Clients1.py
@@ -1,16 +1,21 @@
 #Importar las librerias
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import MinMaxScaler
 
 #Datos para el arbol
-datos = pd.DataFrame({'Servicios':['Internet', 'Internet','Television','Television','Internet','Internet','Television','Internet'],
-                      'HorasAfectacion':[8,6,4,2,7,6,5,1],
-                      'Retiro':[1,1,0,0,1,1,0,0]})
+datos = pd.DataFrame({'nombre':['Cliente1','Cliente2','Cliente3','Cliente4','Cliente5','Cliente6','Cliente7','Cliente8'],
+                      'servicios':['Internet', 'Internet','Television','Television','Internet','Internet','Television','Internet'],
+                      'horasAfectacion':[8,6,4,2,7,6,5,1],
+                      'retiro':[1,1,0,0,1,1,0,0]})
 #Conversion de los datos
-datos['Servicios'] = datos['Servicios'].map({'Internet':0, 'Television':1})
+datos['servicios'] = datos['servicios'].map({'Internet':0, 'Television':1})
 
-x = datos[['Servicios','HorasAfectacion']]
-y = datos['Retiro']
+x = datos[['servicios','horasAfectacion']]
+y = datos['retiro']
+
+scaler = MinMaxScaler()
+x = scaler.fit_transform(x)
 
 #Entrenamiendo del modelo
 model = DecisionTreeClassifier()
@@ -21,6 +26,10 @@ y_pred = model.predict(x)
 
 ClientsWithdrawals = (y_pred == 1).sum()
 NotClientsWithdrawals = (y_pred == 0).sum()
+
+for nombre,pred in zip (datos["nombre"],y_pred):
+    estado = "se retira" if pred == 1 else "no se retira"
+    print(f'{nombre}: {estado}')
 
 #Imprimir el estado de los clientes
 print(f'Clientes que se retiraran del servicio: {ClientsWithdrawals}')
